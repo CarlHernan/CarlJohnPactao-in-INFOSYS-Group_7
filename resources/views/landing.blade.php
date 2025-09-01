c31b8479e094fa973194794dc777f68f54cb52b7
imaginarycjay <imaginarycjay@gmail.com>
2025-09-01 15:23:53 +0800

    landing page v1.1

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -4,12 +4,12 @@
             class="min-h-screen relative flex flex-col items-center justify-center pt-16 text-center gap-8 p-0 md:px-6 md:p-0">
             <h1 class="text-emerald-900 text-2xl md:text-3xl lg:text-6xl max-w-6xl mt-8 ">PingganPH: Simply The Best
                 Karinderya in Kabacan!</h1>
-            <x-menuButton href="/menu">Explore Our Menu</x-menuButton>
+            <h2 class="lg:text-2xl  font-bold ">Todays Paboritos:</h2>
             <img src="{{asset('/images/hero_img1.webp')}}" alt="hero image" loading="lazy"
                  class="w-9/10 max-h-96 object-cover mt-2 rounded-md shadow-lg">
         </section>
 
-        <section class="bg-[#F6B75C] py-12 px-6 lg:px-20 flex flex-col lg:flex-row items-center lg:items-start gap-10">
+        <section class=" bg-custom-yellow front py-12 px-6 lg:px-20 flex flex-col lg:flex-row items-center lg:items-start gap-10">
             <!-- Left Content -->
             <div class="lg:w-1/2 space-y-6">
                 <!-- Headline -->
